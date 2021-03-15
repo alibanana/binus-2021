4b2f8d8deced27757fe608985d8041959740503d
Alifio Rasyid <official.alifio@gmail.com>
2021-03-15 11:43:32 +0700

    Initial Commit - Create Project

---
+++ kelompok-1/src/main/java/com/blibli/kafkaredis/kelompok1/Kelompok1Application.java
@@ -0,0 +1,13 @@
+package com.blibli.kafkaredis.kelompok1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kelompok1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kelompok1Application.class, args);
+	}
+
+}
